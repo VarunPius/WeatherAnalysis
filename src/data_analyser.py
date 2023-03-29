faf524a60bdd79745f83feaa4c443dbe9dc3cc4d
Varun Pius Rodriguus <wraith@test.com>
2023-03-28 21:43:38 -0700

    Post comment changes

--- src/data_analyser.py
+++ src/data_analyser.py
@@ -3,7 +3,7 @@
 #                                                                                                                                                    #
 # data_parser.py                                                                                                                                     #
 # Author(s): Varun Pius Rodrigues                                                                                                                    #
-# About: Get data from API and store in local directory                                                                                              #
+# About: Process data stored in local directory and return results                                                                                   #
 ######################################################################################################################################################
 
 
@@ -13,6 +13,8 @@
 
 # System Libraries
 import os
+import datetime
+import logging
 
 # Internal modules
 from src import basedir, datadir, confdir
@@ -21,69 +23,102 @@ from src import basedir, datadir, confdir
 import pandas as pd
 
 
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Configurations goes here
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+# Logging configurations
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+logging.basicConfig(level=logging.INFO, encoding='UTF-8', format='%(levelname)s: %(message)s')
+logging.info('Main start')
+
+
+######################################################################################################################################################
+# Code starts here
+######################################################################################################################################################
+
+'''
+Method to analyse dataset and return the location, date and temperature of the highest temperatures reported by location and month.
+Basically, I grouped by `location` and found date with the highest temperature, and display them together.
+The output will be location, the highest temperature for the city as well as date when the temperature was reported
+'''
 def get_city_with_max_temp(df):
+    logging.info('Aggregating on location and month level for highest temp')
     df1 = df.copy()
-    df1['max_temp'] = df1.groupby('city')['temp'].transform(lambda x: x.rank(ascending = False))
-    df1 = df1[['city', 'dt', 'temp']].loc[df1.max_temp == 1] 
+    #df1['Date'] = df1.dt.to_datetime
+    df1['DtConverted'] = pd.to_datetime(df1['dt'])
+    df1['month'] = df1['DtConverted'].dt.month
+    logging.info('Month extracted from date')
+
+    df1['max_temp'] = df1.groupby(['city', 'month'])['temp'].transform(lambda x: x.rank(ascending = False))
+    df1 = df1[['city', 'dt', 'temp']].loc[df1.max_temp == 1]
+    logging.info('Final dataframe_1 with filtered columns done')
 
     # Results
     #result_df1 = df1.to_string(index=False)
-    result_df1 = df1.to_json(orient = 'records')
+    result_df1 = df1.to_json(orient = 'records')        # Converting Dataframe to JSON format for HTTP response
     print(result_df1)
 
     return result_df1
 
 
+'''
+Method to analyse dataset and return JSON containing the average temperature, Minimum temperature, 
+    location of minimum temperature, and location of maximum temperature per day.
+The output will be date, avergage temperature, Minimum temperature, as well as location of minimum and maximum temperatures for that date
+'''
 def get_weather_agg(df):
+    logging.info('Aggregating on daily level')
     df2 = df.copy()
     
+    logging.info('Aggregation 2 starts here ')
     df2['avg_temp'] = df2.groupby('dt')['temp'].transform(lambda x: x.mean())
-    #df2['avg_temp'] = df2.groupby('dt')['temp'].agg('mean')
     df2['min_temp'] = df2.groupby('dt')['temp'].transform(lambda x: x.min())
     df2['max_temp'] = df2.groupby('dt')['temp'].transform(lambda x: x.max())
-    df2['min_temp_loc'] = df2.city.loc[df2.min_temp == df2.temp] 
-    df5 = df2.city.loc[df2.min_temp == df2.temp] 
-    print(df5)
 
+    logging.info('Finding cities with max and min temperature')
+    df2['min_temp_loc'] = df2.city.loc[df2.min_temp == df2.temp] 
     df2['max_temp_loc'] = df2.city.loc[df2.max_temp == df2.temp]
 
     # Selecting only necessary columns
+    logging.info('Filtering necessary columns')
     df2 = df2[['dt', 'avg_temp', 'min_temp', 'max_temp', 'min_temp_loc', 'max_temp_loc']]
-    print("Debug 1:")
     print(df2)
 
     # Deleting duplicate columns with Nan values
+    logging.info('Deleting columns with Nan values (they dont have either the highest or lowest values)')
     df2.dropna(subset=['min_temp_loc', 'max_temp_loc'], how='all', inplace=True)
-    print("Debug 2:")
     print(df2)
 
-
     # Dataframe for reporting cities with max temp and min temp
+    logging.info('Merging the rows with max and min temp city values')
     df_temp_city = df2.groupby(['dt']).agg(
                     {'min_temp_loc': lambda x: x.dropna().iloc[0], 
                     'max_temp_loc': lambda x: x.dropna().iloc[0]})
-    print("Debug 3:")
     print(df_temp_city)
     
     # Numeric columns
-    df_agg = df2[['dt', 'avg_temp', 'min_temp', 'max_temp']]
+    logging.info('deleying duplicates for aggregation columns')
+    df_agg = df2[['dt', 'avg_temp', 'min_temp']]
     df_agg.drop_duplicates(inplace=True)
-    print("Debug 4:")
     print(df_agg.sort_values('dt'))
 
     final_df = df_agg.merge(df_temp_city, on='dt')
 
-    print("Debug 5:")
+    logging.info('Final values sorted by date')
     print(final_df.sort_values('dt'))
     
     # Results
     #result_df2 = final_df.to_string(index=False)
-    result_df2 = final_df.to_json(orient = 'records')
+    result_df2 = final_df.to_json(orient = 'records')   # Converting Dataframe to JSON format for HTTP response
     print(result_df2)
 
     return result_df2
 
 
+'''
+Alternative approach for above method
+'''
 def get_weather_agg_alt(df):
     df2 = df.copy()
     
@@ -112,12 +147,14 @@ def get_weather_agg_alt(df):
 
 
 def process_dataframe():
+    logging.info('Runner for fetching results')
     stage_data_file = datadir + "/staging/city_weather_data.json"
     df = pd.read_json(stage_data_file)
 
     result_df1 = get_city_with_max_temp(df)
     result_df2 = get_weather_agg(df)
 
+    logging.info('Results parsed and returning as JSON')
     return result_df1, result_df2
 
 
--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,39 @@
+######################################################################################################################################################
+# Code Info                                                                                                                                          #
+#                                                                                                                                                    #
+# routes.py                                                                                                                                          #
+# Author(s): Varun Pius Rodrigues                                                                                                                    #
+# About: Routes for FastAPI are written here                                                                                                         #
+######################################################################################################################################################
+
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Library Imports goes here
+# -------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+# Internal modules
 from src import app
 from src import data_parser, data_analyser
 
+
+######################################################################################################################################################
+# Code starts here
+######################################################################################################################################################
+
+'''
+Route for Home URL
+Base URL: localhost:8001
+'''
 @app.get("/")
 async def home():
     return {"Hello":"World"}
 
+
+'''
+Route to get data from OpenWeather
+API Request: GET
+URI: localhost:8001/getdata
+'''
 @app.get("/getdata")
 async def get_data():
     print("Inside get data")
@@ -12,6 +41,12 @@ async def get_data():
     print("done get data")
     return {"Status":"Data Obtained"}
 
+
+'''
+Route to get results from storage
+API Request: GET
+URI: localhost:8001/getresults
+'''
 @app.get("/getresults")
 async def get_results():
     print("Inside get results")
